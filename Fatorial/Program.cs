a4ddec4f56d86c9924661eb76894ffb371152eb9
maikeliani <maikel.iani31@gmail.com>
2023-03-05 22:06:14 -0300

    Add project files.

---
+++ Fatorial/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numero, fat, contador;
+        Console.WriteLine( " Informe o numero para o calculo: ");
+        numero = int.Parse( Console.ReadLine() );
+        fat = numero;
+        for( contador = (numero -1); contador >= 1; contador -- )
+        {
+            fat *= contador;
+        }
+
+        if(numero == 0)
+        {
+            fat = 1;
+        }
+               
+        Console.WriteLine($"fatorial de {numero} é : {fat}");
+    }
+}
